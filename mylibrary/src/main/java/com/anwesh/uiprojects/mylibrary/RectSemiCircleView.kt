252f12023f432978a81312340c513087b5cbda8b
Anwesh43 <anweshthecool0@gmail.com>
2020-10-13 01:01:26 +0530

    adding constants and libraries

--- mylibrary/src/main/java/com/anwesh/uiprojects/mylibrary/RectSemiCircleView.kt
+++ mylibrary/src/main/java/com/anwesh/uiprojects/mylibrary/RectSemiCircleView.kt
@@ -3,3 +3,30 @@ package com.anwesh.uiprojects.mylibrary
 /**
  * Created by anweshmishra on 13/10/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Path
+import android.graphics.Color
+import android.content.Context
+
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 2.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val colors : Array<Int> = arrayOf(
+        "#F44336",
+        "#2196F3",
+        "#FFC107",
+        "#3F51B5",
+        "#FF5722"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val deg : Float = 180f
